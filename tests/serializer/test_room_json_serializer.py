89498cdb45cf9bebe62c33c0b8838d09e03398c3
Joe Lezala <jlezala@varde.com>
2019-05-17 09:34:45 -0500

    Step 2 Serializers

---
+++ tests/serializer/test_room_json_serializer.py
@@ -0,0 +1,14 @@
+import json
+import uuid
+
+from rentomatic.domain import room as r
+from rentomatic.serializers import room_json_serializer as ser
+
+def test_serialize_domain_rule():
+    code = uuid.uuid4()
+    room = r.Room(code, size=200, price=10, longitude=-0.09998975,
+                  latitude=51.75436293)
+
+    expected_json = """{{"code": "{}", "size": 200, "price": 10, "latitude": 51.75436293, "longitude": -0.09998975}}""".format(code)
+    json_room = json.dumps(room, cls=ser.RoomJsonEncoder)
+    assert json_room == expected_json
