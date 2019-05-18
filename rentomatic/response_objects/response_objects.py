35f2afc5dbbc6b3f7294e431cd0db6852a49e880
RedPenguin101 <james.masterson101@gmail.com>
2019-05-19 00:39:50 +0800

    step 8 requests and responses part 2

--- rentomatic/response_objects/response_objects.py
+++ rentomatic/response_objects/response_objects.py
@@ -1,6 +1,47 @@
+class ResponseFailure:
+    PARAMETERS_ERROR = 'ParametersError'
+    RESOURCE_ERROR = 'ResourceError'
+    SYSTEM_ERROR = 'SystemError'
+
+    def __init__(self, type, message):
+        self.type = type
+        self.message = self._format_message(message)
+
+    def _format_message(self, msg):
+        if isinstance(msg, Exception):
+            return "{}: {}".format(msg.__class__.__name__, "{}".format(msg))
+        return msg
+
+    @property
+    def value(self):
+        return {'type':self.type, 'message':self.message}
+
+    @classmethod
+    def build_from_invalid_request_object(cls, request_object):
+        message = "\n".join(["{}: {}".format(err['parameter'], err['message'])
+            for err in request_object.errors])
+        return cls(cls.PARAMETERS_ERROR, message)
+
+    @classmethod
+    def build_resource_error(cls, message):
+        return cls(cls.RESOURCE_ERROR, message)
+
+    @classmethod
+    def build_system_error(cls, message):
+        return cls(cls.SYSTEM_ERROR, message)
+
+    @classmethod
+    def build_parameters_error(cls, message):
+        return cls(cls.PARAMETERS_ERROR, message)
+
+    def __bool__(self):
+        return False
+
 class ResponseSuccess:
+    SUCCESS = 'Success'
     def __init__(self, value=None):
         self.value = value
+        self.type = self.SUCCESS
 
     def __bool__(self):
         return True
--- tests/response_objects/test_response_objects.py
+++ tests/response_objects/test_response_objects.py
@@ -1,5 +1,107 @@
-from rentomatic.response_objects import response_objects as res
+import pytest
 
+from rentomatic.response_objects import response_objects as res
+from rentomatic.request_objects import room_list_request_object as req
+
+
+@pytest.fixture
+def response_value():
+    return {'key':['value1','value2']}
+
+
+@pytest.fixture
+def response_type():
+    return 'ResponseError'
+
+@pytest.fixture
+def response_message():
+    return 'This is a response message'
 
 def test_response_success_true():
     assert bool(res.ResponseSuccess()) is True
+
+def test_response_success_has_type_and_value(response_value):
+    '''instantiate response from value and check whether the type is success
+    and the value of the response object is the same as the value passed in'''
+    response = res.ResponseSuccess(response_value)
+
+    '''type of response object is used as the indicator of success or failure
+    represented as a string'''
+    assert response.type == res.ResponseSuccess.SUCCESS
+    assert response.value == response_value
+
+def test_response_failure_is_false(response_type, response_message):
+    assert bool(res.ResponseFailure(response_type, response_message)) is False
+
+
+def test_response_failure_has_type_and_message(
+        response_type, response_message):
+    response = res.ResponseFailure(response_type, response_message)
+
+    assert response.type == response_type
+    assert response.message == response_message
+
+def test_response_failure_contains_value(response_type, response_message):
+    response = res.ResponseFailure(response_type, response_message)
+
+    assert response.value == {'type':response_type, 'message':response_message}
+
+def test_response_failure_initialisation_with_exception():
+    response = res.ResponseFailure(response_type,
+        Exception('Just an error message'))
+
+    assert bool(response) is False
+    assert response.type == response_type
+    assert response.message == 'Exception: Just an error message'
+
+
+def test_response_failure_from_empty_invalid_requst_object():
+    response = res.ResponseFailure.build_from_invalid_request_object(
+            req.InvalidRequestObject())
+
+    assert bool(response) is False
+    assert response.type == res.ResponseFailure.PARAMETERS_ERROR
+
+
+def test_response_failure_from_invalid_request_object_with_errors():
+    request_object = req.InvalidRequestObject()
+    request_object.add_error('path', 'Is mandatory')
+    request_object.add_error('path', "can't be blank")
+
+    response = res.ResponseFailure.build_from_invalid_request_object(
+            request_object)
+    assert bool(response) is False
+    assert response.type == res.ResponseFailure.PARAMETERS_ERROR
+    assert response.message == "path: Is mandatory\npath: can't be blank"
+
+
+def test_response_failure_build_resource_error():
+    '''if a response can't be returned because resources aren't available,
+    a suitable response failure should be returned'''
+
+    response = res.ResponseFailure.build_resource_error('test message')
+
+    assert bool(response) is False
+    assert response.type == res.ResponseFailure.RESOURCE_ERROR
+    assert response.message == 'test message'
+
+def test_response_failure_build_system_error():
+    '''if a response can't be returned because of a system error,
+    a suitable response failure should be returned'''
+
+    response = res.ResponseFailure.build_system_error('test message')
+
+    assert bool(response) is False
+    assert response.type == res.ResponseFailure.SYSTEM_ERROR
+    assert response.message == 'test message'
+
+
+def test_response_failure_build_parameters_error():
+    '''if a response can't be returned because of a paramters error,
+    a suitable response failure should be returned'''
+
+    response = res.ResponseFailure.build_parameters_error('test message')
+
+    assert bool(response) is False
+    assert response.type == res.ResponseFailure.PARAMETERS_ERROR
+    assert response.message == 'test message'
